00f54db8bbe9fce5201c7468e07d191a377eca30
Imanuel Yusuf Setio Budi <yusuf.setiobudi2006@gmail.com>
2026-06-15 14:30:10 +0700

    fix : fix the logo and add 2 navbar menu (solo and clan)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,19 +1,30 @@
-<nav x-data="{ open: false }" class="bg-typing-surface border-b border-gray-800">
+<nav x-data="{ open: false }" class="border-b border-gray-800 bg-typing-surface">
     <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="flex items-center shrink-0">
                     <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-typing-text" />
+                        <x-application-logo class="block w-auto fill-current h-9 text-typing-text" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 text-white sm:-my-px sm:ms-10 sm:flex">
+                    {{-- Clan --}}
                     <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
-                        {{ __('Type') }}
+                        {{ __('Clan') }}
+                    </x-nav-link>
+
+                    {{-- Solo --}}
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        {{ __('Solo') }}
+                    </x-nav-link>
+
+                    {{-- Multiplayer --}}
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        {{ __('Multiplayer') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -23,15 +34,15 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
-                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-typing-muted bg-typing-surface hover:text-typing-text focus:outline-none transition ease-in-out duration-150">
+                            class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 transition duration-150 ease-in-out border border-transparent rounded-md text-typing-muted bg-typing-surface hover:text-typing-text focus:outline-none">
                             @auth
-                                <div class="font-medium text-base text-typing-text">{{ Auth::user()->name }}</div>
+                                <div class="text-base font-medium text-typing-text">{{ Auth::user()->name }}</div>
                             @else
-                                <div class="font-medium text-base text-typing-text">Tamu</div>
+                                <div class="text-base font-medium text-typing-text">Tamu</div>
                             @endauth
 
                             <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                <svg class="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg"
                                     viewBox="0 0 20 20">
                                     <path fill-rule="evenodd"
                                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
@@ -70,10 +81,10 @@
             </div>
 
             <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
+            <div class="flex items-center -me-2 sm:hidden">
                 <button @click="open = ! open"
-                    class="inline-flex items-center justify-center p-2 rounded-md text-typing-muted hover:text-typing-text hover:bg-gray-800 focus:outline-none focus:bg-gray-800 focus:text-typing-text transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                    class="inline-flex items-center justify-center p-2 transition duration-150 ease-in-out rounded-md text-typing-muted hover:text-typing-text hover:bg-gray-800 focus:outline-none focus:bg-gray-800 focus:text-typing-text">
+                    <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
                             stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M4 6h16M4 12h16M4 18h16" />
@@ -97,8 +108,8 @@
         <div class="pt-4 pb-1 border-t border-gray-800">
             @auth
                 <div class="px-4">
-                    <div class="font-medium text-base text-typing-text">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-typing-muted">{{ Auth::user()->email }}</div>
+                    <div class="text-base font-medium text-typing-text">{{ Auth::user()->name }}</div>
+                    <div class="text-sm font-medium text-typing-muted">{{ Auth::user()->email }}</div>
                 </div>
 
                 <div class="mt-3 space-y-1">
@@ -119,7 +130,7 @@
                 </div>
             @else
                 <div class="px-4">
-                    <div class="font-medium text-base text-typing-text">Tamu</div>
+                    <div class="text-base font-medium text-typing-text">Tamu</div>
                 </div>
                 <div class="mt-3 space-y-1">
                     <x-responsive-nav-link :href="route('login')">
