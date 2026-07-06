6391446eef807d41466947a2c5d2d275172f3078
Jason Wijaya <jasonwijaya2006@gmail.com>
2026-07-06 09:00:24 +0700

    chore(ui): update application title and favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'UeType') }}</title>
+    <title>{{ config('app.name', 'UEType') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon/favicon-uetype.png') }}">
 
     <!-- Fonts: Space Grotesk untuk UI/heading, JetBrains Mono untuk area mengetik -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
